ef844a76f4c1b9cf8170ac18fae42b1328030685
ArtemHW <kagirovartem272@gmail.com>
2023-10-12 22:26:24 +0200

    update

---
+++ GamingConsoleF401/Core/Inc/mainCpp.hpp
@@ -0,0 +1,26 @@
+/*
+ * mainCpp.hpp
+ *
+ *  Created on: Sep 28, 2023
+ *      Author: Artem
+ */
+
+#ifndef INC_MAINCPP_HPP_
+#define INC_MAINCPP_HPP_
+
+#include <stdio.h>
+#include <stdint.h>
+
+void mainCpp();
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+	void mainCpp_call();
+#ifdef __cplusplus
+}
+#endif
+
+
+#endif /* INC_MAINCPP_HPP_ */
